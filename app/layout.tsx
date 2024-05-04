39527dbafccf12d56eb376a174d70e73dad6c64c
v4nn4 <romflorentz@gmail.com>
2024-05-04 14:04:00 +0200

    Try fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Analytics } from "@vercel/analytics/react";
 
