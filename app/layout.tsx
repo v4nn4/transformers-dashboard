84cff1fa9337b5bcf16e74bb66be774ea5d5c1e9
v4nn4 <romflorentz@gmail.com>
2024-05-04 13:54:18 +0200

    Try fix css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import "../styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
