0a7677758cb862073f46b47a1ae33158a3845e6d
v4nn4 <romflorentz@gmail.com>
2024-05-04 13:52:37 +0200

    Fix ThemeProvider import + arxiv abs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
-
-import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
